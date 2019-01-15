e2f4f47c049390f7dd841f09a13175464ed4ae64
pankona <yosuke.akatsuka@gmail.com>
2019-01-15 17:35:03 +0900

    update profile script

--- cmd/ki/profile.sh
+++ cmd/ki/profile.sh
@@ -1,28 +1,28 @@
 #!/bin/bash -e
 
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 0 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 1 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 2 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 3 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 4 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 5 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 10 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 20 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 30 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 40 ../..
-#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -c 50 ../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 0 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 1 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 2 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 3 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 4 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 5 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 10 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 20 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 30 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 40 ../../../..
+#sudo sh -c "echo 1 > /proc/sys/vm/drop_caches" && ./ki -with-profile -c 50 ../../../..
 
 # make page cache
-./ki -c 0 ../.. &>/dev/null
+./ki -c 0 ../../../.. &>/dev/null
 
-./ki -c 0 ../..
-./ki -c 1 ../..
-./ki -c 2 ../..
-./ki -c 3 ../..
-./ki -c 4 ../..
-./ki -c 5 ../..
-./ki -c 10 ../..
-./ki -c 20 ../..
-./ki -c 30 ../..
-./ki -c 40 ../..
-./ki -c 50 ../..
+./ki -with-profile -c 0 ../../../..
+./ki -with-profile -c 1 ../../../..
+./ki -with-profile -c 2 ../../../..
+./ki -with-profile -c 3 ../../../..
+./ki -with-profile -c 4 ../../../..
+./ki -with-profile -c 5 ../../../..
+./ki -with-profile -c 10 ../../../..
+./ki -with-profile -c 20 ../../../..
+./ki -with-profile -c 30 ../../../..
+./ki -with-profile -c 40 ../../../..
+./ki -with-profile -c 50 ../../../..
